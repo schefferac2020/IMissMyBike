315aa135a9f97c9e18918ba55c507a5e17bc4eca
drewskis <drewskis@umich.edu>
2020-10-11 20:55:33 -0400

    Did a bit of work

--- craigslist.py
+++ craigslist.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import TimeoutException
 
 from bs4 import BeautifulSoup
 import urllib.request
+from urllib.request import Request, urlopen
 #from send_email import test_sendmail
 import time
 from text_unidecode import unidecode
@@ -18,13 +19,36 @@ class CraigsScraper(object):
 		self.radius = radius
 
 		self.url = f"https://{location}.craigslist.org/search/search_distance={radius}&postal={postal}&max_price={max_price}"
+		
+		self.driver = webdriver.Firefox(executable_path='/Users/drewscheffer/Downloads/geckodriver')
+		self.delay = 3
 
-		self.driver = webdriver.Firefox()
-		self.delay = 5
+	def load_craigslist_url(self):
+		self.driver.get(self.url)
+		try:
 
+			wait = WebDriverWait(self.driver, self.delay)
+			wait.until(EC.presence_of_element_located((By.ID, "searchform")))
+			print("page is ready")
+		except TimeoutException:
+			print("Loading took too much time")
 
-	def test(self):
-		print(self.url)
+	def extract_post_titles(self):
+		all_posts = self.driver.find_elements_by_class_name("result-row")
+		post_title_list = []
+		for post in all_posts:
+			print(post.text)
+			post_title_list.append(post.text)
+		return post_title_list
+
+	def extract_post_urls(self):
+		results = self.driver.find_elements_by_css_selector('.result-row')
+		a = results[0].find_element_by_css_selector('.result-info')
+		text = unidecode(a.find_element_by_tag_name('a').text)
+		print(text)
+	
+	
+	
 
 location = "annarbor"
 postal = "48109"
@@ -32,4 +56,6 @@ max_price = 900
 radius = 20
 
 scraper = CraigsScraper(location, postal, max_price, radius)
-scraper.test()
+scraper.load_craigslist_url()
+#scraper.extract_post_titles()
+scraper.extract_post_urls()
